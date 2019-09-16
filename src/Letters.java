68c24dc3b46c9f07149cd10bd5f4b4549274eef1
schoeeva000 <schoeeva000@CAM-LAB-26.bgsd.k12.wa.us>
2019-09-16 07:53:45 -0700

    inProgress

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,30 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	public static void letter1() {
+		System.out.println("Dear Mom,\n");
+		common();
+		System.out.println("How is Dad doing? I miss you guys dearly, even though I have only");
+		System.out.println("been at school for a little less than an hour.  Thank you for");
+		System.out.println("making lunch for me today.\n");
+		System.out.println("With love,\n\nEvan");
+	}
+	
+	public static void letter2() {
+		System.out.println("Dear cousin Liam,\n");
+		common();
+		System.out.println("I can't believe it's been so long since I have been out to see you");
+		System.out.println("for Christmas! I am excited to go sledding with you down the massive");
+		
+	}
+	
+	public static void common() {
+		System.out.println("I am doing very well. Just yesterday I got back from a retreat,");
+		System.out.println("and on that retreat I helped to watch the little kids. In school,");
+		System.out.println("I am doing very well,  and so far my grades are great! I have the");
+		System.out.println("perfect Christmas present for you, I can't wait to get it!\n");
+	}
 }
